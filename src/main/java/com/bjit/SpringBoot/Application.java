ceb413d58b6bde6c91c228abef4985da747d3630
Tasmin Afroz <tasmin.afroz@bjitgroup.com>
2018-02-06 17:26:52 +0600

    6_2_18

--- src/main/java/com/bjit/SpringBoot/Application.java
+++ src/main/java/com/bjit/SpringBoot/Application.java
@@ -2,8 +2,10 @@ package com.bjit.SpringBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication 
+
 public class Application {
 
 	public static void main(String[] args) {
